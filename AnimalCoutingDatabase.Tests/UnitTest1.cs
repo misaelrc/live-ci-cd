e806594c1ac0a8a8bfc3f5c432e3c1ca06658871
Misael Rosa <37905523+misaelrc@users.noreply.github.com>
2022-10-21 18:36:28 -0300

    Add tests

---
+++ AnimalCoutingDatabase.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace AnimalCoutingDatabase.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(1 == 1);
+        }
+    }
+}
